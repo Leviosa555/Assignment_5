ea6788968c4ef2f39702c82010f25a1023b41d09
JB007 <114020469+Leviosa555@users.noreply.github.com>
2025-06-08 14:01:08 -0400

    task2.py

---
+++ task2.py
@@ -0,0 +1,8 @@
+numbers = list(range(1, 11))
+extracted = numbers[:5]
+
+reversed_extracted = extracted[::-1]
+
+print(f"Original list: {numbers}")
+print(f"Extracted first five elements: {extracted}")
+print(f"Reversed extracted elements: {reversed_extracted}")
